c1b16c6be84d95a60b519fdd194ceb26738f0f5f
amazing <admin@amazingboy.cn>
2021-09-23 17:43:50 +0800

    slice

--- demoslice/demoslice.go
+++ demoslice/demoslice.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var a = []int{1, 2, 3, 4, 5}
+	a = append(a, 6)
+	fmt.Println(a)
+	// 要删除索引为2的元素
+	a = append(a[:2], a[3:]...)
+
+	fmt.Println(a)
 }
